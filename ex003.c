52396bc5a49f13fab9aba2ca648a4d64fda9c178
Kemoel Amorim Miranda <62153868+kemoelamorim@users.noreply.github.com>
2023-02-14 20:36:22 -0300

    list 3

--- ex003.c
+++ ex003.c
@@ -1,7 +1,15 @@
 // 3. Desenvolva uma função recursiva que receba como entrada um número inteiro positivo N e retorne o seguinte cálculo: 1 + 1/2 + 1/3 + 1/4 + ... + 1/N.
 #include <stdio.h>
 
+float somaFracaoRecursiva(int n);
+
 int main(){
-    printf("hello");
+    printf("%.2f", somaFracaoRecursiva(2));
 }
 
+float somaFracaoRecursiva(int n){
+  if(n == 1){
+    return 1;
+  }
+  return 1.0/n + somaFracaoRecursiva(n - 1);
+}
